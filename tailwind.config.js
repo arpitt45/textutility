0b6c944ee3085f87e56317605b90e326fc5934f6
arpitt45 <arpitkashyap903@gmail.com>
2024-03-17 17:48:08 +0530

    Config TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
